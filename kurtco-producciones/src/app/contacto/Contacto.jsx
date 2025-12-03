96ef3bb0907a9812f28e559de16e4a4e76c43ca5
joy-amorin <amorinkaren2021@gmail.com>
2025-12-03 15:09:49 -0300

    style(contacto): improve layaout

--- kurtco-producciones/src/app/contacto/Contacto.jsx
+++ kurtco-producciones/src/app/contacto/Contacto.jsx
@@ -1,44 +1,35 @@
-import React from 'react';
-
-const ContactSection = () => {
+export default function ContactSection() {
   return (
-    <section id="contacto" className="w-full bg-black text-[#f4f4f4] py-20">
-      <div className="container mx-auto px-8 lg:px-0 flex flex-col items-center text-center mt-6">
-        
-        <h2 className="text-5xl font-black mb-4 tracking-tight">CONTACTO</h2>
-        <div className="w-24 h-1 bg-red-600 mb-10"></div>
-        
-        <p className="text-2xl md:text-3xl font-semibold text-[#f4f4f4] mb-12 relative after:content-[''] after:block after:w-12 after:h-[2px] after:bg-red-600 after:mx-auto after:mt-3">
-          Hablemos de tu proyecto
+    <section id="contacto" className="relative w-full min-h-screen bg-black py-20 lg:py-32 px-6 md:px-12 lg:px-20">
+      <div className="max-w-7xl mx-auto text-center flex flex-col items-center gap-3 px-6">
+
+        <h2  className="text-3xl md:text-4xl font-black text-[#f4f4f4] mb-6"
+           >
+          Contacto
+        </h2>
+
+        <p className="text-base md:text-lg text-[#f4f4f4]">
+            📱 095 414 520 &nbsp; | &nbsp; 📧 info@kurtco.com &nbsp; | &nbsp;
+            📷{" "}
+            <a
+              href="https://www.instagram.com/kurtco"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="hover:underline"
+            >
+              @kurtco
+            </a>
         </p>
 
-        <div className="space-y-6 text-lg md:text-xl">
-          <p>📞 095 414 520</p>
-          <p>📧 info@productora.com</p>
-          <p>📷 {" "}
-            <a
-            href='https://www.instagram.com/kurtco.pro/'
-            target='blank'
-            rel='noopener noreferrer'
-            className='hover:underline'
-            >
-            @kurtco.pro
-            </a>
-          </p>
-        </div>
-
         <a
-          href="https://wa.me/95414520"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="mt-14 inline-block bg-red-600 text-[#f4f4f4] font-bold text-lg px-8 py-3 rounded-lg hover:bg-red-700 transition-all duration-300 cursor-pointer"
-        >
-          ESCRIBINOS POR WHATSAPP
-        </a>
-
+        href="https://wa.me/59895414520"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="mt-2 inline-block px-5 py-2 rounded-xl bg-red-600 text-[#f4f4f4] font-semibold hover:bg-[#ff003c] transition"
+      >
+        ESCRIBINOS POR WHATSAPP
+      </a>
       </div>
     </section>
   );
-};
-
-export default ContactSection;
+}
