82141ef1af27b27e44da225c6008853638399899
joy-amorin <amorinkaren2021@gmail.com>
2025-12-10 11:31:22 -0300

    style: add new photos for hero section and chage positions in layout

--- kurtco-producciones/src/data/HeroGalery.js
+++ kurtco-producciones/src/data/HeroGalery.js
@@ -1,7 +1,12 @@
 export const HeroGallery = [
     { src: '/hero-photos/hero-1.jpg', alt: "banda de rock en vivo"},
+    { src: '/hero-photos/hero-2.jpg', alt: "banda de rock en vivo"},
     { src: '/hero-photos/hero-3.jpg', alt: "banda de rock en vivo"},
+    { src: '/hero-photos/hero-4.jpg', alt: "banda de rock en vivo"},
     { src: '/hero-photos/hero-5.jpg', alt: "banda de rock en vivo"},
+    { src: '/hero-photos/hero-6.jpg', alt: "banda de rock en vivo"},
+    { src: '/hero-photos/hero-7.jpg', alt: "banda de rock en vivo"},
+    { src: '/hero-photos/hero-8.jpg', alt: "banda de rock en vivo"},
     { src: '/hero-photos/hero-9.jpg', alt: "banda de rock en vivo"},
     { src: '/hero-photos/hero-10.jpg', alt: "banda de rock en vivo"},
     { src: '/hero-photos/hero-11.jpg', alt: "banda de rock en vivo"},
@@ -15,20 +20,15 @@ export const HeroGallery = [
     { src: '/hero-photos/hero-19.jpg', alt: "banda de rock en vivo"},
     { src: '/hero-photos/hero-20.jpg', alt: "banda de rock en vivo"},
     { src: '/hero-photos/hero-21.jpg', alt: "banda de rock en vivo"},
-    { src: '/hero-photos/hero-10.jpg', alt: "banda de rock en vivo"},
-    { src: '/hero-photos/hero-11.jpg', alt: "banda de rock en vivo"},
-    { src: '/hero-photos/hero-12.jpg', alt: "banda de rock en vivo"},
-    { src: '/hero-photos/hero-13.jpg', alt: "banda de rock en vivo"},
-    { src: '/hero-photos/hero-14.jpg', alt: "banda de rock en vivo"},
-    { src: '/hero-photos/hero-15.jpg', alt: "banda de rock en vivo"},
+    { src: '/hero-photos/hero-22.jpg', alt: "banda de rock en vivo"},
+    { src: '/hero-photos/hero-23.jpg', alt: "banda de rock en vivo"},
     { src: '/hero-photos/hero-16.jpg', alt: "banda de rock en vivo"},
-    { src: '/hero-photos/hero-17.jpg', alt: "banda de rock en vivo"},
-    { src: '/hero-photos/hero-14.jpg', alt: "banda de rock en vivo"},
-    { src: '/hero-photos/hero-15.jpg', alt: "banda de rock en vivo"},
-    { src: '/hero-photos/hero-16.jpg', alt: "banda de rock en vivo"},
-    { src: '/hero-photos/hero-17.jpg', alt: "banda de rock en vivo"},
-    { src: '/hero-photos/hero-16.jpg', alt: "banda de rock en vivo"},
-
+    { src: '/hero-photos/hero-21.jpg', alt: "banda de rock en vivo"},
+    { src: '/hero-photos/hero-27.jpg', alt: "banda de rock en vivo"},
+    { src: '/hero-photos/hero-28.jpg', alt: "banda de rock en vivo"},
+    { src: '/hero-photos/hero-29.jpg', alt: "banda de rock en vivo"},
+    { src: '/hero-photos/hero-30.jpg', alt: "banda de rock en vivo"},
+    
 
     
     
